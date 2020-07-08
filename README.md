4299112a4a6231013b745c83c6da1f66d35c8e86
guoyichao <1948188829@qq.com>
2020-07-08 23:38:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# epay
+
+#### 项目介绍
+一个支付结算项目
+
+#### 博客教程
+
+[Java第三方支付接入案例（支付宝）](https://www.cnblogs.com/xifengxiaoma/p/10107635.html)
+
+#### 软件架构
+软件架构说明
+
+
+#### 安装教程
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 使用说明
+
+1. xxxx
+2. xxxx
+3. xxxx
+
+#### 参与贡献
+
+1. Fork 本项目
+2. 新建 Feat_xxx 分支
+3. 提交代码
+4. 新建 Pull Request
+
+1. 
---
+++ src/epay-service/src/main/java/com/louis/epay/EpayApplication.java
@@ -0,0 +1,12 @@
+package com.louis.epay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootAcpplication
+public class EpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpayApplication.class, args);
+	}
+}
